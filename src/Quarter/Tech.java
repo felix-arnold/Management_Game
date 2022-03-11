f35d1f48277e8f3bb471c2e18c5918bb55e2bb59
GuillaumeLi <81261803+GuillaumeLi@users.noreply.github.com>
2022-03-11 16:00:41 +0100

    Update Tech class

--- src/Quarter/Tech.java
+++ src/Quarter/Tech.java
@@ -2,24 +2,46 @@ package Quarter;
 
 public class Tech {
     private String techType;
+    private Tech previousTech;
     private boolean unlocked;
+    private boolean canUnlock;
     private final int requiredRessource1;
     private final int requiredRessource2;
 
-    public Tech(String techType, int ressource1, int ressource2) {
+    public Tech(String techType, int ressource1, int ressource2, Tech previousTech) {
         this.techType = techType;
         unlocked = false;
+        canUnlock = false;
         requiredRessource1 = ressource1;
         requiredRessource2 = ressource2;
+        this.previousTech = previousTech;
     }
 
     public boolean isUnlocked() {
         return unlocked;
     }
 
-    public void techUpdate(int ressource1, int ressource2) {
-        if((requiredRessource1==ressource1)&&(requiredRessource2==ressource2)) {
-            unlocked = true;
-        }
+    public void setUnlocked(boolean bool) {
+        unlocked = bool;
+    }
+
+    public void setCanUnlock(boolean bool) {
+        canUnlock = bool;
+    }
+
+    public boolean canUnlock() {
+        return canUnlock;
+    }
+
+    public Tech getPreviousTech() {
+        return previousTech;
+    }
+
+    public int getRequiredRessource1() {
+        return requiredRessource1;
+    }
+
+    public int getRequiredRessource2() {
+        return requiredRessource2;
     }
 }
